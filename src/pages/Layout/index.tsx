bc57db4050c097a4cd2eede23671603e05d42fe6
arthuralveso <arthuralves91@hotmail.com>
2022-03-23 20:46:05 -0300

    adiciona layout da pagina

---
+++ src/pages/Layout/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Container, LeftContainer, RightContainer, TitleSection, HeaderSection } from './styles'
+import leftArrow from '../../assets/back-arrow.svg'
+import cardIcon from '../../assets/card-icon.svg'
+import rightArrow from '../../assets/right-arrow.svg'
+import confirmIcon from '../../assets/confirm-icon.svg'
+import { CardForm } from '../../components/CardForm'
+
+export function Layout() {
+    return (
+        <Container>
+            <LeftContainer>
+                <span>
+                    <img src={leftArrow} alt="voltar" />
+                    <p>Alterar forma de pagamento</p>
+                </span>
+
+                <TitleSection>
+                    <div>
+                        <img src={cardIcon} alt="card" />
+                    </div>
+
+                    <p>Adicione um novo cartão de crédito</p>
+                </TitleSection>
+
+
+            </LeftContainer>
+
+            <RightContainer>
+                <header>
+                    <HeaderSection hasImage={true}>
+                        <span>
+                            <div>
+                                <img src={confirmIcon} alt="confirm" />
+                            </div>
+                            <p>Carrinho</p>
+                        </span>
+                    </HeaderSection>
+
+                    <img src={rightArrow} alt="confirm" />
+
+                    <HeaderSection hasImage={false}>
+                        <span>
+                            <div>
+                                <p>2</p>
+                            </div>
+                            <p>Carrinho</p>
+                        </span>
+                    </HeaderSection>
+
+                    <img src={rightArrow} alt="confirm" />
+
+                    <HeaderSection hasImage={false}>
+                        <span>
+                            <div>
+                                <p>3</p>
+                            </div>
+                            <p>Carrinho</p>
+                        </span>
+                    </HeaderSection>
+
+                </header>
+
+                <CardForm />
+
+
+
+            </RightContainer>
+        </Container>
+    )
+}
---
+++ src/pages/Layout/styles.ts
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    
+`;
+interface HeaderSectionProps {
+    hasImage: boolean
+}
+
+
+export const HeaderSection = styled.div<HeaderSectionProps>`
+
+    span {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            
+            div {
+                margin-right: 8px;
+            }
+        }
+
+        span > div {
+            min-height: 20px;
+            min-width: 20px;
+            border-radius: 50%;
+            background: ${({ hasImage }) => hasImage && '#4BDE95'};
+            border: 1px solid #4BDE95;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+`;
+
+export const LeftContainer = styled.div`
+    height: 100vh;
+    width: 325px;
+    background: var(--primary);
+
+    color: var(--text-light);
+
+    padding: 50px 8px;
+
+    span {
+        display: flex;
+        align-items: center;
+
+        p { 
+            font-size: 13px;
+            margin-left: 11px;
+        }
+    }
+`;
+
+export const TitleSection = styled.div`
+        display: flex;
+        align-items: center;
+        margin-top: 51px;
+        height: 50px;
+
+        div {
+            min-height: 50px;
+            min-width: 50px;
+            border-radius: 50%;
+            border: 2px solid var(--text-light);
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            
+        }
+
+        img {
+            width: 30px;
+        }
+
+        p { 
+            font-size: 24px;
+            margin-left: 28px;
+        }
+`;
+
+
+
+export const RightContainer = styled.div`
+       color: var(--primary);
+       width: 1000px;
+       
+       header {
+        
+        display: flex;
+        font-weight: 400;
+        font-size: 20px;
+        justify-content: space-between;
+        padding: 46px 62px 0 0;
+       }
+
+   
+`;
+
+
